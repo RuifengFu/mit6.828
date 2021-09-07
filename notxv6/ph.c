9286307fb5b7be96c8b23d837a891af2666c74e3
Ruifeng Fu <frf2000@tju.edu.cn>
2021-09-07 01:11:40 -0700

    lab-thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,10 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+
+pthread_mutex_t locks[NBUCKET];            // declare a lock
+
+
 double
 now()
 {
@@ -51,7 +55,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&locks[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&locks[i]);
   }
 }
 
